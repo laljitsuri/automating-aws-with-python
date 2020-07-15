c5e1f5e868425ed88ae57d69579859a1aff36f83
Laljit Suri <laljitsuri@Laljits-MBP.gateway>
2020-07-15 22:46:43 +1000

    EC2 creation script

---
+++ 02-notifon/ec2_example.py
@@ -0,0 +1,56 @@
+import boto3
+
+session = boto3.Session(profile_name='pythonAutomation')
+ec2=session.resource('ec2')
+
+img = ec2.Image('ami-020d764f9372da231')
+
+img_name = img.name
+
+ami_name = 'amzn-ami-hvm-2018.03.0.20200602.1-x86_64-gp2'
+
+filters = [{'Name': 'name', 'Values': [ami_name]}]
+
+list(ec2.images.filter(Owners=['amazon'], Filters=filters))
+
+ec2_client = session.client('ec2')
+key_pairs=ec2_client.describe_key_pairs()
+key_pairs_list=key_pairs.get('KeyPairs')
+key_rsa=None
+for k in key_pairs_list:
+    if k.get('KeyName') == 'id_rsa':
+        key_rsa = k
+
+instances = ec2.create_instances(ImageId=img.id,
+                                MinCount=1,
+                                MaxCount=1,
+                                InstanceType='t2.micro',
+                                KeyName='id_rsa'
+                            )
+inst = instances[0]
+inst.wait_until_running()
+inst.reload()
+inst.public_dns_name
+inst.security_groups
+
+sg = ec2.SecurityGroup(inst.security_groups[0]['GroupId'])
+sg.authorize_ingress(IpPermissions=[{'FromPort':22,
+                                    'ToPort':22,
+                                    'IpProtocol':'TCP',
+                                    'IpRanges':[{'CidrIp':'141.168.87.232/32'}]
+                                }]
+                    )
+
+sg.authorize_ingress(IpPermissions=[{'FromPort':80,
+                                    'ToPort':80,
+                                    'IpProtocol':'TCP',
+                                    'IpRanges':[{'CidrIp':'0.0.0.0/0'}]
+                                }]
+                    )
+
+ssh ec2-user@ec2-13-211-159-125.ap-southeast-2.compute.amazonaws.com
+sudo yum -y update
+sudo yum -y install httpd
+sudo chkconfig httpd on
+sudo service httpd start
+control-D
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Repository for Automating AWS with Python project. Check out cool modules for automating various aws resources
 
 
-# webtron
+# 01-webtron
 Directory for project to sync local directory to s3
 
 # Features
@@ -12,3 +12,10 @@ Currently webtron has following Features
 3) Create and setup a bucket
 4) Sync directory tree from system for a given directory to given bucket in s3
 5) Set AWS profile with option --profile=<profile_name>
+
+
+# 02-notifon
+Notifon is a project to notify sloack users of changes to AWS account using Cloudwatch events
+
+### Features
+Notifon has following features:
