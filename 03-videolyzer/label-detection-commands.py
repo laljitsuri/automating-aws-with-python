a0ef70085f27cd403196da404456b07955ee97d4
Laljit Suri <laljitsuri@Unknown.gateway>
2020-07-19 20:03:20 +1000

    Trigger rekoginition job in Lamda function

--- 03-videolyzer/label-detection-commands.py
+++ 03-videolyzer/label-detection-commands.py
@@ -20,3 +20,12 @@ result.keys()
 result['JobStatus']
 result.get('VideoMetadata')
 len(result['Labels'])
+
+# S3 event and getting bucket and key from events
+event = {'Records': [{'eventVersion': '2.1', 'eventSource': 'aws:s3', 'awsRegion': 'ap-southeast-2', 'eventTime': '2020-07-19T08:56:54.484Z', 'eventName': 'ObjectCreated:CompleteMultipartUpload', 'userIdentity': {'principalId': 'AWS:AIDAZUTP5XJNK4C66OIA3'}, 'requestParameters': {'sourceIPAddress': '141.168.87.232'}, 'responseElements': {'x-amz-request-id': 'B39BE0118F2FD439', 'x-amz-id-2': 'CzerFt0E8qlLny/vSgqUX7FOx8xsTaFf2jonBcIgL9UUD3/PTjU6GBvrgkKvhogUzCRGtpCwW2/YRDegscZUiAPHEIzDDcPa'}, 's3': {'s3SchemaVersion': '1.0', 'configurationId': '23058e34-dfb2-44cd-80de-ab741e229848', 'bucket': {'name': 'laljitvideoanalyzer', 'ownerIdentity': {'principalId': 'A3SNFGB7D2C7ZG'}, 'arn': 'arn:aws:s3:::laljitvideoanalyzer'}, 'object': {'key': 'production_ID_4426380.mp4', 'size': 20649866, 'eTag': 'a22ad94c309236ed3d4d7df4925ff3c1-3', 'sequencer': '005F140ACBC58598B5'}}}]}
+event
+rec = event.get('Records')[0]
+rec.get('s3').get('bucket').get('name')
+rec.get('s3').get('object').get('key')
+import urllib.parse
+urllib.parse.unquote_plus(rec.get('s3').get('object').get('key'))
--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -1,3 +1,25 @@
+import urllib.parse
+import boto3
+
+def start_label_detection(bucket, key):
+    rekognition_client = boto3.client('rekognition')
+    response = rekognition_client.start_label_detection(
+                    Video={
+                        'S3Object': {
+                            'Bucket':bucket,
+                            'Name':key
+                            }
+                        }
+                    )
+    print(response)
+    return
+
+
 def start_processing_video(event, context):
-    print(event)
+    for record in event.get('Records'):
+        start_label_detection(
+            record.get('s3').get('bucket').get('name'),
+            urllib.parse.unquote_plus(record.get('s3').get('object').get('key'))
+        )
+
     return
