b978d354a9fbdf86a2a51d00cb1e505c9626695e
Laljit Suri <laljitsuri@Laljits-MacBook-Pro.local>
2020-07-20 22:12:18 +1000

    Save data from rekoginition job in dynamodb table

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -17,7 +17,7 @@ def start_label_detection(bucket, key):
                         'RoleArn': os.environ['REKOGNITION_ROLE_ARN']
                     }
                 )
-    print(response)
+    
     return
 
 def get_video_labels(job_id):
@@ -36,9 +36,35 @@ def get_video_labels(job_id):
 
     return response
 
+def make_item(data):
+    if isinstance(data, dict):
+        return {k:make_item(v) for k,v in data.items()}
+
+    if isinstance(data, list):
+        return [make_item(v) for v in data]
+
+    if isinstance(data, float):
+        return str(data)
+
+    return data
+
 
 def put_labels_in_db(data, video_name, video_bucket):
-    pass
+    del data['ResponseMetadata']
+    del data['JobStatus']
+
+    data['videoName'] = video_name
+    data['videoBucket'] = video_bucket
+
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ['DYNAMODB_TABLE_NAME']
+    videos_table = dynamodb.Table(table_name)
+
+    data = make_item(data)
+
+    videos_table.put_item(Item=data)
+
+    return
 
 
 def start_processing_video(event, context):
@@ -59,7 +85,7 @@ def handle_label_detection(event, context):
         s3_object = message_json['Video']['S3ObjectName']
 
         response = get_video_labels(job_id)
-        print("Response from rekognition job is "+str(response))
+
         put_labels_in_db(response, s3_object, s3_bucket)
 
     return
